53595cb309accf435e1f814db7a2574ad4fcea2f
elfin8er <elfin8er@hotmail.com>
2014-06-12 10:54:31 -0700

    Updated to fix console()

--- src/ExamplePlugin/MainClass.php
+++ src/ExamplePlugin/MainClass.php
@@ -15,17 +15,17 @@ use pocketmine\utils\TextFormat;
 class MainClass extends PluginBase implements Listener, CommandExecutor{
 
 	public function onLoad(){
-		$this->getLogger()->info(TextFormat::WHITE . "[ExamplePlugin] I've been loaded!");
+		$this->getLogger()->info(TextFormat::WHITE . "I've been loaded!");
 	}
 
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->getServer()->getScheduler()->scheduleRepeatingTask(new BroadcastPluginTask($this), 120);
-		$this->getLogger()->info(TextFormat::DARK_GREEN . "[ExamplePlugin] I've been enabled!");
+		$this->getLogger()->info(TextFormat::DARK_GREEN . "I've been enabled!");
     }
 
 	public function onDisable(){
-		$this->getLogger()->info(TextFormat::DARK_RED . "[ExamplePlugin] I've been disabled!");
+		$this->getLogger()->info(TextFormat::DARK_RED . "I've been disabled!");
 	}
 
 	public function onCommand(CommandSender $sender, Command $command, $label, array $args){
