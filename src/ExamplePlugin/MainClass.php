a8134963ba91b45e06c894b2874bd6b825f179d4
RandomAltThingy <goldowl37@gmail.com>
2015-10-24 16:49:14 +0100

    Updated some things

--- src/ExamplePlugin/MainClass.php
+++ src/ExamplePlugin/MainClass.php
@@ -5,6 +5,7 @@ namespace ExamplePlugin;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerRespawnEvent;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
@@ -31,6 +32,11 @@ class MainClass extends PluginBase implements Listener{
 		switch($command->getName()){
 			case "example":
 				$sender->sendMessage("Hello ".$sender->getName()."!");
+				$sender->sendMessage("Some information about you:");
+				$sender->sendMessage("Display name: " . $sender->getDisplayName());
+				$sender->sendMessage("Health: " . $sender->getHealth());
+				$sender->sendMessage("UUID: " . $sender->getUniqueId());
+				$sender->sendMessage("First played: " . $sender->getFirstPlayed()):
 				return true;
 			default:
 				return false;
@@ -45,5 +51,9 @@ class MainClass extends PluginBase implements Listener{
 	 */
 	public function onSpawn(PlayerRespawnEvent $event){
 		Server::getInstance()->broadcastMessage($event->getPlayer()->getDisplayName() . " has just spawned!");
+	}
+	
+	public function onJoin(PlayerJoinEvent $event){
+		$event->setJoinMessage($event->getPlayer()->getDisplayName() . " has joined the server!");
 	}
 }
